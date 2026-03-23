56a128a8c343d2aeb2d5c95e4b9bdbf5c58ee150
etm81608-boop <etm81608@gmail.com>
2026-03-23 08:45:13 -0400

    Update Sidebar.jsx

--- running-plans/src/components/Sidebar.jsx
+++ running-plans/src/components/Sidebar.jsx
@@ -56,6 +56,22 @@ const nav = [
     ),
   },
   {
+    to: '/strength-workouts', label: 'Strength Workouts',
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064" />
+      </svg>
+    ),
+  },
+  {
+    to: '/swim-workouts', label: 'Swim Workouts',
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 15a4 4 0 004 4h9a5 5 0 10-4.9-6H7a4 4 0 00-4 4z" />
+      </svg>
+    ),
+  },
+  {
     to: '/team-grid', label: 'Team Grid',
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
