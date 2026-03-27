600eb24979786ea5e254e408a79671ac2ed761d1
etm81608-boop <etm81608@gmail.com>
2026-03-27 19:00:52 -0400

    Update Layout.jsx

--- running-plans/src/components/Layout.jsx
+++ running-plans/src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
+import Messenger from './Messenger'
 
 export default function Layout() {
   return (
@@ -8,6 +9,7 @@ export default function Layout() {
       <main className="flex-1 overflow-auto">
         <Outlet />
       </main>
+      <Messenger />
     </div>
   )
 }
