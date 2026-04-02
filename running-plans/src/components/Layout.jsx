fef5ec35d5baab3ef366e3cf55a262e5e1542900
etm81608-boop <etm81608@gmail.com>
2026-04-02 06:35:55 -0400

    Update Layout.jsx

--- running-plans/src/components/Layout.jsx
+++ running-plans/src/components/Layout.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
 import Messenger from './Messenger'
+import { MessengerProvider } from '../contexts/MessengerContext'
 
 export default function Layout() {
   return (
-    <div className="flex min-h-screen bg-gray-50">
-      <Sidebar />
-      <main className="flex-1 overflow-auto">
-        <Outlet />
-      </main>
-      <Messenger />
-    </div>
+    <MessengerProvider>
+      <div className="flex min-h-screen" style={{ background: 'linear-gradient(160deg, #f0fdf4 0%, #ecfeff 100%)' }}>
+        <Sidebar />
+        <main className="flex-1 overflow-auto">
+          <Outlet />
+        </main>
+        <Messenger />
+      </div>
+    </MessengerProvider>
   )
 }
